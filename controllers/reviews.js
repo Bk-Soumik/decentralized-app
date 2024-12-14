a446fa6e7b847b610a596bb057772c19409bacc5
Bk_Soumik <gsamick98@gmail.com>
2024-12-14 11:32:07 +0530

    more files

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,24 +1,51 @@
-const Listing = requuire('../models/listing.js');
+// const Listing = requuire('../models/listing.js');
+// const Review = require("../models/review.js");
+
+// module.exports.createReview = async(req,res) => {
+//     let listing = await Listing.findById(req.params.id);
+//     let newReview = new Review(req.body.review);
+//     newReview.author = req.user._id;
+
+//     listing.reviews.push(newReview);
+
+//     await newReview.save();
+//     await listing.save();
+//     req.flash("success", "New Review Created");
+//    res.redirect(`/listings/${listing._id}`);
+// }
+
+// module.exports.destroyReview = async (req,res) => {
+//     let {id, reviewId } = req.params;
+//     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+//     await Review.findByIdAndDelete(reviewId);
+//     req.flash("success", "Deleted Review");
+
+//     res.redirect(`/listings/${id}`);
+// }
+
+const Listing = require('../models/listing.js');
 const Review = require("../models/review.js");
 
-module.exports.createReview = async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
+module.exports.createReview = async (req, res) => {
+    const { id } = req.params; // Access the listing ID from route params
+    const listing = await Listing.findById(id);
+    const newReview = new Review(req.body.review);
     newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
 
     await newReview.save();
     await listing.save();
+
     req.flash("success", "New Review Created");
-   res.redirect(`/listings/${listing._id}`);
-}
+    res.redirect(`/listings/${listing._id}`);
+};
 
-module.exports.destroyReview = async (req,res) => {
-    let {id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+module.exports.destroyReview = async (req, res) => {
+    const { id, reviewId } = req.params; // Access listing ID and review ID from route params
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Deleted Review");
 
+    req.flash("success", "Deleted Review");
     res.redirect(`/listings/${id}`);
-}
+};
--- routes/review.js
+++ routes/review.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("../models/listing.js");
-const {validateReview, isLoggedIn} = require("../middleware.js");
-const reviewController = require("../models/review.js");
+// const express = require("express");
+// const router = express.Router({mergeParams: true});
+// const wrapAsync = require("../utils/wrapAsync.js");
+// const {validateReview, isLoggedIn} = require("../middleware.js");
+// const reviewController = require("../reviews/review.js");
 
-//REVIEW Route
+// //REVIEW Route
+// router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+
+// //DELETE REVIEW ROUTE
+// router.delete("/:reviewId", isLoggedIn, wrapAsync(reviewController.destroyReview));
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router({ mergeParams: true }); // Ensure access to parent route params
+const wrapAsync = require("../utils/wrapAsync.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+
+// Create Review Route
 router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
-//DELETE REVIEW ROUTE
+// Delete Review Route
 router.delete("/:reviewId", isLoggedIn, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
